1dd241b576d25eea0a2d28bf72b6d22bc5f3919a
emhaki <mhmh779@naver.com>
2024-01-14 13:08:57 +0900

    모델 정의

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,26 @@
+from asyncio import AbstractServer
 from django.db import models
 
 # Create your models here.
+class User(AbstractServer):
+    team = models.CharField(max_length=100)
+
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    create_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    is_complete = models.BooleanField(default=False)
+    completed_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class SubTask(models.Model):
+    id = models.AutoField(primary_key=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    team = models.CharField(max_length=100)
+    is_complete = models.BooleanField(default=False)
+    completed_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
